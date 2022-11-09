c8aeb370fe1fbb3e9ed4548b6a36f2953c69daa6
Jojolgenius <jocelyn.hauf@gmail.com>
2022-11-09 16:30:34 +0100

    Prog serv first version

--- Programme_serveur.py
+++ Programme_serveur.py
@@ -2,4 +2,7 @@ from os import listdir
 from os.path import isfile, join
 
 
-fichiers = [f for f in listdir() if isfile(join(monRepertoire, f))]
+fichiers = [f for f in listdir("Questions") if isfile(join("Questions", f))]
+fichier = listdir("Questions")
+print(fichier)
+
