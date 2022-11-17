870b529979ff602d9e439bc97639007150a4ecda
Jojolgenius <jocelyn.hauf@gmail.com>
2022-11-17 18:19:20 +0100

    Modif prog serveur

--- Programme_serveur.py
+++ Programme_serveur.py
@@ -2,6 +2,10 @@
 import requests
 import ressources
 
+# Librairie pour les arbres dom
+import xml.dom.minidom
+
+
 # Gestion des fichiers 
 from os import listdir
 from os.path import isfile, join, splitext
@@ -24,6 +28,39 @@ def waitFile() :
     #Renvoie du fichier à traiter après la boucle
     return fichiers[0]
 
+
+# Fonction qui permet de lire un fichier
+def readXML(fic):
+    # Creation de l'arbre dom depuis le xml
+    tree = xml.dom.minidom.parse("Questions/"+ fic)
+    # Premier element de l'arbre
+    elt = tree.documentElement
+
+    # Recupere le type de requete (Premier element de l'arbre)
+    typereq = elt.getAttribute("typeQuestion")
+
+    #-----------------------------------------------#
+    # Analyse de la requete en fonction de son type #
+    #-----------------------------------------------#
+
+    # Cv depuis les compétences
+    if typereq == "CompFromCv":
+
+        # Init itérateur
+        i = 0
+        # Init la liste des compétences
+        liComp = []
+
+        # Récupere la valeur des elements "competence" (liste) dans l'arbre dom (forme une liste/NodeList)
+        competences = tree.getElementsByTagName("competence")
+
+        # Creation de la liste de competences en variable
+        for comp in competences:
+            liComp.append(comp.childNodes[0].nodeValue)
+    
+    print(liComp)
+
+
 # Init la bdd et les message de requete
 db, req = ressources.ConnectDB()
 
@@ -35,9 +72,11 @@ while(True) :
     print(fic)
 
     # Lecture du fichier
-    fichier = open("Questions/"+fic, "r")
-    print(fichier.read())
-    fichier.close()
+    #fichier = open("Questions/"+fic, "r")
+    #print(fichier.read())
+    #fichier.close()
+
+    readXML(fic)
 
     break
 
